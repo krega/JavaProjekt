296050ffb0ddd03cf9fc1884cd0e96de13f74376
krega <kamil.regoc@gmail.com>
2016-02-20 23:52:39 +0100

    Prawie wszystko

--- Logowanie_2/src/Panel_Logowania.java
+++ Logowanie_2/src/Panel_Logowania.java
@@ -6,56 +6,98 @@
 import javax.swing.JFrame;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  *
  * @author Kamil
  */
-public class Panel_Logowania extends JFrame {
+public class Panel_Logowania extends JFrame implements ActionListener {
 
     public static void main(String[] args) {
         new Panel_Logowania();
     }
+    private JLabel Login;
+    private JLabel Haslo;
+    private JTextArea Login_1 ;
+    private JPasswordField Haslo_1;
+    private JButton OK ;
+   private  JButton Rejestracja;
 
     public Panel_Logowania() {
+        
 
         JFrame Panel = new JFrame("Panel Logowania");
         Panel.setLayout(null);
         Panel.setBounds(400, 400, 400, 400);
-        //JPanel contentPane = (JPanel) Panel.getContentPane();
-
-        //contentPane.add(new JLabel("Haslo"));
-        JLabel Login = new JLabel("Login");
+        Login = new JLabel("Login");
         Login.setLayout(null);
         Login.setBounds(100, 50, 400, 100);
         Panel.add(Login);
-        JLabel Haslo = new JLabel("Haslo");
+        Haslo = new JLabel("Haslo");
         Haslo.setLayout(null);
         Haslo.setBounds(100, 100, 400, 100);
         Panel.add(Haslo);
 
-        JTextArea Login_1 = new JTextArea("Login");
+        Login_1 = new JTextArea("Login");
         Login_1.setLayout(null);
         Login_1.setBounds(200, 92, 100, 17);
         Panel.add(Login_1);
-        JPasswordField Haslo_1 = new JPasswordField();
+        Haslo_1 = new JPasswordField();
         Haslo_1.setLayout(null);
         Haslo_1.setBounds(200, 140, 100, 17);
         Panel.add(Haslo_1);
 
-        JButton OK = new JButton("OK");
+        OK = new JButton("OK");
         OK.setLayout(null);
         OK.setBounds(280, 200, 60, 40);
+        OK.addActionListener (this);
         Panel.add(OK);
 
-        JButton Rejestracja = new JButton("Rejestracja");
+        Rejestracja = new JButton("Rejestracja");
         Rejestracja.setLayout(null);
         Rejestracja.setBounds(20, 200, 120, 40);
         Panel.add(Rejestracja);
+        Rejestracja.addActionListener (this);
 
         Panel.setSize(new Dimension(400, 300));
         Panel.setLocation(50, 300);
         Panel.setVisible(true);
     }
+     @Override
+     public void actionPerformed(ActionEvent e) {
+           Object src = e.getSource();
+            if (src == OK) {
 
+        char[] pass = Haslo_1.getPassword();
+        String passString = new String(pass);
+         OdczytZPliku odczyt=new OdczytZPliku();
+         String LiH=new String(odczyt.Odczyt());
+         LiH=LiH.trim();
+         String LP=new String(Login_1.getText()+passString);
+         
+         
+       if(LiH.equals(LP)){
+           System.out.print("zalogowany");
+       }
+       else{
+            System.out.print("NIEzalogowany   "+LiH+"   "+LP);
+            
+           
+       }
+            }
+           else if (src == Rejestracja){
+               this.setVisible(false);
+                new Panel_Rejestracji().setVisible(true);               
+                
+            }
+         
+     }
 }
+     
+    
+    
+    
+
+
--- Logowanie_2/src/Panel_Rejestracji.java
+++ Logowanie_2/src/Panel_Rejestracji.java
@@ -21,20 +21,17 @@ public class Panel_Rejestracji extends JFrame implements ActionListener {
         new Panel_Rejestracji();
             
     }
-  private  JLabel Login = new JLabel("Login");
-  private  JLabel Haslo = new JLabel("Haslo");
-   private  JTextArea Login_1 = new JTextArea("Login");
-    private JPasswordField Haslo_1 = new JPasswordField();
-   private  JButton OK = new JButton("OK");
-  
- private  char[] pass = Haslo_1.getPassword();
-   String passString = new String(pass);
-    
- 
-  
-  
+    private JLabel Login ;
+    private JLabel Haslo ;
+    private JTextArea Login_1;
+    private JPasswordField Haslo_1;
+    private JButton OK ;
+    private char[] pass ;
+
    
-     
+
+   
+
      
      
      
@@ -42,49 +39,54 @@ public class Panel_Rejestracji extends JFrame implements ActionListener {
      
 
     public Panel_Rejestracji() {
-        OK.addActionListener(this);
-        JFrame Panel = new JFrame("Panel Logowania");
-        Panel.setLayout(null);
-        Panel.setBounds(400, 400, 400, 400);
-        //contentPane.add(new JLabel("Haslo"));
+        
+        
+        
+    Login = new JLabel("Login");
+    Haslo = new JLabel("Haslo");
+    Login_1 = new JTextArea("Login");
+    Haslo_1 = new JPasswordField();
+    OK = new JButton("OK");
 
-        Login.setLayout(null);
-        Login.setBounds(100, 50, 400, 100);
-        Panel.add(Login);
 
-        Haslo.setLayout(null);
-        Haslo.setBounds(100, 100, 400, 100);
-        Panel.add(Haslo);
+    OK.addActionListener (this);
+    JFrame Panel = new JFrame("Panel Logowania");
+    Panel.setLayout (  null);
+    Panel.setBounds ( 400, 400, 400, 400);
+    Login.setLayout ( null);
+    Login.setBounds ( 100, 50, 400, 100);
+    Panel.add (Login);
+    Haslo.setLayout ( null);
+    Haslo.setBounds (100, 100, 400, 100);
+    Panel.add (Haslo);
+    Login_1.setLayout ( null);
+    Login_1.setBounds ( 200, 92, 100, 17);
+    Panel.add (Login_1);
+    Haslo_1.setLayout ( null);
+    Haslo_1.setBounds ( 200, 140, 100, 17);
+    Panel.add (Haslo_1);
+    OK.setLayout ( null);
+    OK.setBounds (280, 200, 60, 40);
+    Panel.add (OK);
+    Panel.setSize ( new Dimension(400, 300));
+    Panel.setLocation (   50, 300);
+    Panel.setVisible (true);
 
-        Login_1.setLayout(null);
-        Login_1.setBounds(200, 92, 100, 17);
-        Panel.add(Login_1);
 
-        Haslo_1.setLayout(null);
-        Haslo_1.setBounds(200, 140, 100, 17);
-        Panel.add(Haslo_1);
-
-        OK.setLayout(null);
-        OK.setBounds(280, 200, 60, 40);
-        Panel.add(OK);
-
-        Panel.setSize(new Dimension(400, 300));
-        Panel.setLocation(50, 300);
-        Panel.setVisible(true);
 
     }
-
+    
     @Override
     public void actionPerformed(ActionEvent e) {
+        pass = Haslo_1.getPassword();
+        String passString = new String(pass);
 
-        Credentials Cr = new Credentials(Login_1.getText(),passString );
+        Credentials Cr = new Credentials(Login_1.getText(), passString);
 
         User nowy = new User(Cr);
         Register reg = new Register(Cr);
         reg.saveNewUser(Cr);
-         System.out.print("aas"+pass);
-      
 
     }
-    
+
 }
